6f9da846b3af16c24039a2f6186e961b1237740e
michaeltorku <michaeltorku5@gmail.com>
2023-11-21 11:04:50 -0500

    linting

--- host.cpp
+++ host.cpp
@@ -35,10 +35,6 @@ void AsyncHostSend(int target_node_id, Path& selected_path, int &reduce_id, std:
 void Host::send(int reduce_id, int data, std::map<int, Host> &host_map, std::map<int, Switch> &switch_map){
     int p_idx = load_balancer::balance(this->paths);
     Path selected_path = paths[p_idx];
-
     int target_node_id = selected_path.upper_node[1] - '0';
     executions.emplace_back(AsyncHostSend, target_node_id, std::ref(selected_path), std::ref(reduce_id), std::ref(all_reduce_map), std::ref(host_map), std::ref(switch_map));
-    // std::thread th(AsyncHostSend, target_node_id, std::ref(selected_path), std::ref(reduce_id), std::ref(all_reduce_map), std::ref(host_map), std::ref(switch_map));
-
-
 }
